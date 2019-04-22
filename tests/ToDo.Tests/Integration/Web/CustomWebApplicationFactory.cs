e8376307d6efb69e83dec0f4aa96467e497e0486
Wouter de Kort <wouter_dk@outlook.com>
2019-04-22 15:06:38 +0200

    Added LaunchDarkley integration

--- tests/ToDo.Tests/Integration/Web/CustomWebApplicationFactory.cs
+++ tests/ToDo.Tests/Integration/Web/CustomWebApplicationFactory.cs
@@ -5,7 +5,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System;
 using System.IO;
-using System.Reflection;
 using ToDo.Core.Interfaces;
 using ToDo.Infrastructure.Data;
 using ToDo.Web;
